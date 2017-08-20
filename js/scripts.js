03031d7fd0b0f8f136934c08a9b0705965ba38eb
Logan <musashimiyamoto@interia.eu>
2017-08-21 01:20:43 +0200

    changes

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var a = prompt('Wpisz wartość a'),
+    b = prompt('Wpisz wartość b'),
+    value = (a * a) + (2 * a * b) - (b * b);
+if (value > 0)
+    alert('wynik dodatni');
+else if (value < 0)
+    alert('wynik ujemny');
+console.log(' (a * a) + (2 * a * b) - (b * b)= ' + value);git status
